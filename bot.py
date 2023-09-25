c355fbf635df97a87053ecf02e8dcee82d42fdad
Githubaswin <67902149+Githubaswin@users.noreply.github.com>
2023-09-25 20:34:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,28 +2,36 @@ import asyncio
 from os import environ
 from pyrogram import Client, filters, idle
 
-API_ID = []
-API_HASH = environ.get("")
-BOT_TOKEN = environ.get("")
-SESSION = environ.get("")
-TIME = 300
+# Ensure that your API_ID, API_HASH, and BOT_TOKEN environment variables are set correctly
+API_ID = int(environ.get("API_ID"))
+API_HASH = environ.get("API_HASH")
+BOT_TOKEN = environ.get("BOT_TOKEN")
+SESSION = environ.get("SESSION")
+TIME = int(environ.get("TIME"))
 GROUPS = []
+for grp in environ.get("GROUPS").split():
+    GROUPS.append(int(grp))
+ADMINS = []
+for usr in environ.get("ADMINS").split():
+    ADMINS.append(int(usr))
 
-START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time. Join our Official Channel @T4TVSeries1</b>"
+START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time Join our Ofiicial Channel @T4TVSeries1</b>"
 
-User = Client(name="user-account",
-              session_string=SESSION,
-              api_id=API_ID,
-              api_hash=API_HASH,
-              workers=300
-              )
+User = Client(
+    name="user-account",
+    session_string=SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    workers=300
+)
 
-Bot = Client(name="auto-delete",
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=300
-             )
+Bot = Client(
+    name="auto-delete",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    workers=300
+)
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
@@ -32,22 +40,20 @@ async def start(bot, message):
 @User.on_message(filters.chat(GROUPS))
 async def delete(user, message):
     try:
-       if message.from_user.id in ADMINS:
-          return
-       else:
-          await asyncio.sleep(TIME)
-          await Bot.delete_messages(message.chat.id, message.id)
+        if message.from_user.id in ADMINS:
+            return
+        else:
+            await asyncio.sleep(TIME)
+            await Bot.delete_messages(message.chat.id, message.id)
     except Exception as e:
-       print(e)
-       
+        print(e)
+
 User.start()
 print("User Started!")
 Bot.start()
 print("Bot Started!")
 
 idle()
-
-# The code below will not be reached unless you forcefully stop the script
 
 User.stop()
 print("User Stopped!")
